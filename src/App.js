7004a2bf227b7251d72f7ce5080ebb936f4c8ad5
devakrons <test.akronltd@gmail.com>
2025-01-11 12:58:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Test from "./test";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+      <Test />
+      <div className="w-full h-36 bg-red-300">Test here</div>
+    </div>
+  );
+}
+
+export default App;
